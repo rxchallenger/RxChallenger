21e449079a5d441be5d9afb7e066f8d460506040
Ahmed-M-Ezzat <Ahmed_M_Ezzat@outlook.com>
2026-06-21 03:31:23 +0300

    Initial commit

--- builds/web/index.service.worker.js
+++ builds/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782001048|76153625';
+const CACHE_VERSION = '1782001758|786594261';
 /** @type {string} */
 const CACHE_PREFIX = 'Rx Challenger-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
