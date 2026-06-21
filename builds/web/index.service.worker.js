503b5b367aa16c9b5ed83dfd24f4c2677f96fd6a
Ahmed-M-Ezzat <Ahmed_M_Ezzat@outlook.com>
2026-06-21 03:06:59 +0300

    Initial commit

--- builds/web/index.service.worker.js
+++ builds/web/index.service.worker.js
@@ -4,14 +4,14 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781998668|13023460724';
+const CACHE_VERSION = '1782000265|14620100115';
 /** @type {string} */
 const CACHE_PREFIX = 'Rx Challenger-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
-const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
+const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = false;
 // Files that will be cached on load.
 /** @type {string[]} */
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
