aab411de93b1698377ddb0b5dd96b861d746d1fa
Ahmed-M-Ezzat <Ahmed_M_Ezzat@outlook.com>
2026-06-24 18:06:46 +0300

    Vercel meta data

--- builds/web/index.service.worker.js
+++ builds/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782247176|16314852146';
+const CACHE_VERSION = '1782313538|1200425425';
 /** @type {string} */
 const CACHE_PREFIX = 'Rx Challenger-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
